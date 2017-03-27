56fb3edbba93c2a645e5ff02adf1f44bda930ef3
人啊！！！ <s20151104690@1.local>
2017-03-27 10:18:32 +0800

    Initial Commit

---
+++ scoreboardTests/scoreboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  scoreboardTests.swift
+//  scoreboardTests
+//
+//  Created by 20151104690 on 17/3/27.
+//  Copyright © 2017年 4690. All rights reserved.
+//
+
+import XCTest
+@testable import scoreboard
+
+class scoreboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
