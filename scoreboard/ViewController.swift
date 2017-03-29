0824ac6cdbb791d15e8898a3d6aaaff9cfaeb668
人啊！！！ <s20151104690@1.local>
2017-03-29 11:44:19 +0800

    动画
    加入了动画的切换

--- scoreboard/ViewController.swift
+++ scoreboard/ViewController.swift
@@ -11,24 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
+    @IBAction func playpicture(_ sender: UIButton) {
+        image.animationImages = (1...4).map{
+            UIImage(named:"ios\($0)")!
+        }
+        
+        /*image.animationImages = [
+            UIImage(named:"ios1")!,
+            UIImage(named:"ios2")!
+        ]
+    
+    */
+    
+       image.contentMode = .scaleAspectFit
+        
+        image.animationDuration = 1
+        image.startAnimating()
+ 
 
-    
-    /*UIImage(contentsOfFile:副本.jpg)
-    let path = Bundle.main.path(forResource: "swift",ofType:"jpg")!
-    let imgNormal = UIImage(contentsOfFile:path)
-     */
-    
-    
-    /*var img = UIImage(named:"副本.jpg")    //初始化图片
-    var vimg = UIImageView(image:img )   //初始化图片View
-    vimg.frame.origin = CGPoint(x:0,y:20)
-    //指定图片显示的位置
-    //vImg.frame = CGRect(x:0,y:20,width:120,height:120);   //指定图片的位置以及显示的大小
-    self.view.addSubview(vImg)*/
 
+
+    }
+    
     override func viewDidLoad ( ) {
         super.viewDidLoad()
-        // Dispose of any resources that can be recreated.
+               //Dispose of any resources that can be recreated.
     }
 }
     
